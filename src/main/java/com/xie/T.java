c93537229522b6475eedef5c3b657fce12d15889
职业无情 <48913733+Monomikey@users.noreply.github.com>
2020-06-14 20:58:19 +0800

    hello

---
+++ src/main/java/com/xie/T.java
@@ -0,0 +1,20 @@
+package com.xie;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class T {
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setVisible(true);
+        frame.setSize(500,500);
+        frame.setResizable(false);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
